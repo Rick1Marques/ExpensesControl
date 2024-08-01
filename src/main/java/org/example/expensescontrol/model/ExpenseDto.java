bcf452a691e29ce6c3d49bef8c022e04729a8cd4
henriquemarques <henriquedms7@gmail.com>
2024-08-01 14:53:45 +0200

    add Expense and ExpenseDto models

---
+++ src/main/java/org/example/expensescontrol/model/ExpenseDto.java
@@ -0,0 +1,15 @@
+package org.example.expensescontrol.model;
+
+import lombok.With;
+
+import java.time.LocalDate;
+
+public record ExpenseDto(
+        String category,
+        String supplier,
+        double amount,
+        boolean cashPayment,
+        String description,
+        LocalDate date
+) {
+}
