b49aab3d85303025a4713d04551a05b168ebd559
henriquemarques <henriquedms7@gmail.com>
2024-08-01 13:59:43 +0200

    Initial commit

---
+++ src/main/java/org/example/expensescontrol/ExpensesControlApplication.java
@@ -0,0 +1,13 @@
+package org.example.expensescontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensesControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpensesControlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/expensescontrol/ExpensesControlApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.expensescontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensesControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
