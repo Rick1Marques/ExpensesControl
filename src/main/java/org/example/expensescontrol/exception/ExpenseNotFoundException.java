30c76cde8c1a07c6ec876d8f28bc55fd03226f82
henriquemarques <henriquedms7@gmail.com>
2024-08-03 07:03:34 +0200

    add exception ExpenseNotFound

---
+++ src/main/java/org/example/expensescontrol/exception/ExpenseNotFoundException.java
@@ -0,0 +1,7 @@
+package org.example.expensescontrol.exception;
+
+public class ExpenseNotFoundException extends RuntimeException{
+    public ExpenseNotFoundException(String message){
+        super(message);
+    }
+}
