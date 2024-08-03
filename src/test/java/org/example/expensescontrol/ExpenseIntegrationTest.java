e9975c22fa3183301445de6326a7dbe28f15733c
henriquemarques <henriquedms7@gmail.com>
2024-08-03 07:21:24 +0200

    add integration test for PUT /api/expenses

--- src/test/java/org/example/expensescontrol/ExpenseIntegrationTest.java
+++ src/test/java/org/example/expensescontrol/ExpenseIntegrationTest.java
@@ -1,5 +1,7 @@
 package org.example.expensescontrol;
 
+import org.example.expensescontrol.DB.ExpenseRepo;
+import org.example.expensescontrol.model.Expense;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -10,6 +12,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.time.LocalDate;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @SpringBootTest
@@ -19,18 +23,21 @@ public class ExpenseIntegrationTest {
     @Autowired
     MockMvc mvc;
 
+    @Autowired
+    ExpenseRepo expenseRepo;
+
     @Test
     @DirtiesContext
-    void getExpenses() throws Exception{
+    void getExpenses() throws Exception {
         mvc.perform(MockMvcRequestBuilders
-                .get("/api/expenses"))
+                        .get("/api/expenses"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().json("[]"));
     }
 
     @Test
     @DirtiesContext
-    void addExpense() throws Exception {
+    void postExpense() throws Exception {
 
         mvc.perform(MockMvcRequestBuilders
                         .post("/api/expenses")
@@ -58,6 +65,50 @@ public class ExpenseIntegrationTest {
 
                         """))
                 .andExpect(jsonPath("$.id").exists());
+    }
+
+    @Test
+    @DirtiesContext
+    void putExpense() throws Exception {
+
+        Expense expense = new Expense(
+                "1",
+                "food",
+                "liferando",
+                30.70,
+                false,
+                "",
+                LocalDate.of(2024,5,20)
+        );
+        expenseRepo.save(expense);
+
+        mvc.perform(MockMvcRequestBuilders
+                        .put("/api/expenses")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                                        {
+                                                        "id": "1",
+                                                        "category":"food",
+                                                        "supplier":"china rest",
+                                                        "amount":35.70,
+                                                        "cashPayment":false,
+                                                        "description":"",
+                                                        "date":"2024-05-20"
+                                                        }
+                                """)
+                )
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json("""
+                                                {
+                                                "id": "1",
+                                                "category":"food",
+                                                "supplier":"china rest",
+                                                "amount":35.70,
+                                                "cashPayment":false,
+                                                "description":"",
+                                                "date":"2024-05-20"
+                                                }
+                        """));
     }
 
 }
