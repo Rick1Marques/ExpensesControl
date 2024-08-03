1f44a1eb0978d01d0bd3e722e1069f3fc73f0f89
henriquemarques <henriquedms7@gmail.com>
2024-08-03 07:09:55 +0200

    implement ExceptionHandler for ExpenseNotFoundException

--- src/main/java/org/example/expensescontrol/exception/GlobalException.java
+++ src/main/java/org/example/expensescontrol/exception/GlobalException.java
@@ -9,6 +9,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalException {
 
 
+    @ExceptionHandler(ExpenseNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorObject handleExpenseNotFoundException(ExpenseNotFoundException exception){
+        return new ErrorObject(exception.getMessage());
+    }
+
     @ExceptionHandler(RuntimeException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorObject handleRunTimeException(RuntimeException exception){
